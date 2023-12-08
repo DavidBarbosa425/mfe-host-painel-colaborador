539ecf07db5b3e35f1b23466a8353403adf6104c
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2023-12-08 12:44:53 -0300

    BrowserModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
-import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    SharedModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { MaterialModule } from "./libraries/material/material.module";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from '@angular/platform-browser'
 
 
 
@@ -12,13 +13,15 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
   exports : [
     FlexLayoutModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule
 
   ],
   imports : [
     FlexLayoutModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule
   ]
 })
 
