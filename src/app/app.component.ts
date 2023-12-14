5e0b540f27a5d87c288d252f096647a879d8f3b5
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2023-12-14 14:22:58 -0300

    host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+<<<<<<< Updated upstream
 export class AppComponent {
   title = 'mfe-host';
+=======
+export class AppComponent implements OnInit {
+
+  isColaborador: boolean = false
+  isGestor:boolean = false
+  isExpandir: boolean = false
+
+  ngOnInit(): void {
+    this.isColaborador = true
+  }
+
+  expandir(){
+    this.isExpandir = !this.isExpandir
+  }
+>>>>>>> Stashed changes
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared/shared.module";
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,5 +12,20 @@ import { SharedModule } from "./shared/shared.module";
   ],
   providers: [],
   bootstrap: [AppComponent],
+=======
+import { CustomFormularioModule } from "./shared/template/form/custom-formulario.module";
+import { PainelColaboradorModule } from "./features/painel-colaborador.module";
+
+@NgModule({
+    declarations: [AppComponent],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        AppRoutingModule,
+        SharedModule,
+        CustomFormularioModule,
+        PainelColaboradorModule
+    ]
+>>>>>>> Stashed changes
 })
 export class AppModule {}
